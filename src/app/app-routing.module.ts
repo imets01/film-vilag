3dcc5444942a4b7c5386799bd0c10cf9c3d0147d
imets01 <100612206+imets01@users.noreply.github.com>
2023-05-14 19:07:00 +0200

    delete comments

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-// import { MovieComponent } from './movie/movie.component';
 import { SearchComponent } from './search/search.component';
-// import { SeriesComponent } from './series/series.component';
 import { DetailsComponent } from './details/details.component';
 import { PersonComponent } from './person/person.component';
 
@@ -11,7 +9,6 @@ const routes: Routes = [
   {path: '', redirectTo: 'home', pathMatch: 'full'},
   {path: 'home/movies', component: HomeComponent},
   {path: 'home/series', component: HomeComponent},
-  // {path: 'series', component: SeriesComponent},
   {path: 'search', component: SearchComponent},
   {path: 'movie/:id', component: DetailsComponent},
   {path: 'series/:id', component: DetailsComponent},
